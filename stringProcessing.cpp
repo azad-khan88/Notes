afac8591c1b632850ca3d9751d0909a543782230
azad-khan88 <inbox.azadkhan@gmail.com>
2019-05-12 13:28:06 +0200

    string Processing

---
+++ stringProcessing.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    string str="Discover interesting projects and people to populate your personal news feed";
+
+    // add in the end
+    str.append(" Azad Khan");
+
+    str.push_back('a');
+
+    //insert githut helps in the start
+    str.insert(0,"Github helps ");
+    str.erase(4,5);
+
+    //replace github with Github
+    str.replace(str.find("interesting"),11,"Amazon AWS");
+    cout<<str;
+
+
+
+}
